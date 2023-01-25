1c647e540bdc11af80822aa45c757220cfc87abd
Asdrúbal Mejía López <asdrumejia19@gmail.com>
2023-01-25 15:20:55 -0500

    api/src/models(Recipe models updated)

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -1,12 +1,54 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('recipe', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "recipe",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+
+      summary: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+
+      dishTypes: {
+        type: DataTypes.STRING,
+      },
+
+      healthScore: {
+        type: DataTypes.INTEGER,
+      },
+
+      steps: {
+        type: DataTypes.STRING,
+      },
+
+      image: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+
+      createdInDb: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+      },
     },
-  });
+
+    {
+      timestamps: false,
+    }
+  );
 };
